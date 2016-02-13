4f0848988264db078f02666881e5819cc3146c77
Dong Feng <dfeng@adobe.com>
2016-02-13 07:23:01 -0800

    Clean up.

--- haskell/haskell/ViewController.m
+++ haskell/haskell/ViewController.m
@@ -11,18 +11,15 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-
-    // Do any additional setup after loading the view.
 }
 
-- (void)setRepresentedObject:(id)representedObject {
+- (void)setRepresentedObject:(id)representedObject
+{
     [super setRepresentedObject:representedObject];
-
-    // Update the view, if already loaded.
 }
-
 
 - (IBAction)showAlert:(id)sender
 {
