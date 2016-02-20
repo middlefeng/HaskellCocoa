894b1faef399674b22e5a14d7abdfaf5cb7ddd1e
middleware <middle.fengdong@gmail.com>
2016-02-20 14:38:29 -0800

    Custom view.

---
+++ haskell/haskell/View.m
@@ -0,0 +1,18 @@
+//
+//  View.m
+//  haskell
+//
+//  Created by middleware on 2/20/16.
+//  Copyright © 2016 dfeng. All rights reserved.
+//
+
+#import "View.h"
+
+@implementation View
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    [super drawRect:dirtyRect];
+}
+
+@end
