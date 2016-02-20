ddd9b59aba34ae67ee8f5b2db2a41f1ed9678e94
middleware <middle.fengdong@gmail.com>
2016-02-20 14:44:46 -0800

    Draw outter frame.

--- haskell/haskell/View.m
+++ haskell/haskell/View.m
@@ -13,6 +13,17 @@
 - (void)drawRect:(NSRect)dirtyRect
 {
     [super drawRect:dirtyRect];
+    
+    NSColor* color = [NSColor grayColor];
+    [color set];
+    
+    NSRect rect = [self frame];
+    rect.origin.x += 20;
+    rect.origin.y += 20;
+    rect.size.width -= 40;
+    rect.size.height -= 40;
+    NSBezierPath* path = [NSBezierPath bezierPathWithRoundedRect:rect xRadius:5 yRadius:5];
+    [path stroke];
 }
 
 @end
