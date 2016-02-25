285922440d78765eb5da0626c46675dcc0ff0e2c
Dong Feng <dfeng@adobe.com>
2016-02-24 18:06:52 -0800

    Undo/Redo in framework.

---
+++ HaskellCocoa/AppFoundation/HModelUndoRedo.hs
@@ -0,0 +1,64 @@
+
+
+
+{-# LANGUAGE ForeignFunctionInterface #-}
+
+
+
+module AppFoundation.HModelUndoRedo
+(
+    HistoryModel(..)
+,   modelCanUndo
+,   modelUpdateUndo
+,   modelCanRedo
+,   modelUpdateRedo
+)
+where
+
+
+
+data HistoryModel a = HistoryModel
+        {
+            historyModelCurrent :: a
+        ,   historyModelPrev :: Maybe (HistoryModel a)
+        ,   historyModelNext :: Maybe (HistoryModel a)
+        }
+
+
+
+modelUpdateUndo :: HistoryModel a -> HistoryModel a
+modelUpdateUndo m =
+    case (historyModelPrev m) of
+        Nothing -> m
+        Just prev ->
+            HistoryModel {
+                             historyModelCurrent = historyModelCurrent prev
+                         ,   historyModelPrev = historyModelPrev prev
+                         ,   historyModelNext = Just m
+                         }
+
+
+modelCanUndo :: HistoryModel a -> Bool
+modelCanUndo m = case (historyModelPrev m) of Nothing -> False
+                                              _       -> True
+
+
+
+
+modelUpdateRedo :: HistoryModel a -> HistoryModel a
+modelUpdateRedo m =
+    case (historyModelNext m) of
+        Nothing -> m
+        Just next ->
+            HistoryModel {
+                             historyModelCurrent = historyModelCurrent next
+                         ,   historyModelPrev = Just m
+                         ,   historyModelNext = historyModelNext next
+                         }
+
+
+modelCanRedo :: HistoryModel a -> Bool
+modelCanRedo m = case (historyModelNext m) of Nothing -> False
+                                              _       -> True
+
+
