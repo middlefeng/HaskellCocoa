046713edb9736dfdfbadf36465f4c176920bc8a5
middleware <middle.fengdong@gmail.com>
2016-02-27 10:56:46 -0800

    Content view.

---
+++ Samples/haskell/haskell/ContentView.h
@@ -0,0 +1,13 @@
+//
+//  ContentView.h
+//  haskell
+//
+//  Created by middleware on 2/27/16.
+//  Copyright © 2016 dfeng. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ContentView : NSView
+
+@end
---
+++ Samples/haskell/haskell/ContentView.m
@@ -0,0 +1,18 @@
+//
+//  ContentView.m
+//  haskell
+//
+//  Created by middleware on 2/27/16.
+//  Copyright © 2016 dfeng. All rights reserved.
+//
+
+#import "ContentView.h"
+
+@implementation ContentView
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    [super drawRect:dirtyRect];
+}
+
+@end
